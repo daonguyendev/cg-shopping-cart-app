32e5b6714c6ef0f30b3b0d96be841cd61977720c
daonguyendev <daonguyen.dev@gmail.com>
2023-07-22 10:04:35 +0700

    Update mock data to api call

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -20,12 +20,7 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
 
   return (
     <Card className="h-100">
-      <Card.Img
-        variant="top"
-        src={imgUrl}
-        height="200px"
-        style={{ objectFit: "cover" }}
-      />
+      <Card.Img variant="top" src={imgUrl} height="200px" style={{ objectFit: "cover" }}/>
       <Card.Body className="d-flex flex-column">
         <Card.Title className="d-flex justify-content-between align-items-baseline mb-4">
           <span className="fs-2">{name}</span>
@@ -38,24 +33,16 @@ export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
             </Button>
           ) : (
             <div
-              className="d-flex align-items-center flex-column"
-              style={{ gap: ".5rem" }}
-            >
-              <div
-                className="d-flex align-items-center justify-content-center"
-                style={{ gap: ".5rem" }}
-              >
+              className="d-flex align-items-center flex-column" style={{ gap: ".5rem" }}>
+              <div className="d-flex align-items-center justify-content-center" style={{ gap: ".5rem" }}>
                 <Button onClick={() => decreaseCartQuantity(id)}>-</Button>
-                <div>
-                  <span className="fs-3">{quantity}</span> in cart
-                </div>
+                <div><span className="fs-3">{quantity}</span> in cart</div>
                 <Button onClick={() => increaseCartQuantity(id)}>+</Button>
               </div>
               <Button
                 onClick={() => removeFromCart(id)}
                 variant="danger"
-                size="sm"
-              >
+                size="sm">
                 Remove
               </Button>
             </div>
--- src/hooks/useCartItemData.ts
+++ src/hooks/useCartItemData.ts
@@ -6,7 +6,8 @@ export const useGetCartItemDatas = () => {
   const [data, setData] = useState<CartItemData[]>([]);
 
   const getData = async () => {
-    const { data } = await axios.get("./data/items.json");
+    // const { data } = await axios.get("./data/items.json"); //use for mock data
+    const { data } = await axios.get("http://localhost:8080/products/get-all"); //use for api call
     setData(data);
   };
 
